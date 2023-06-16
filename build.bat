c4fee8a663f5e05ac4c89cc59781f9190f87a34f
Aaron Tasker <aaron.tasker@gmail.com>
2023-06-16 17:28:59 +0200

    Build script change

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 python update.py
 pyinstaller -F monitor/main.py
-copy dist\main.exe C:\Users\aaron\Dropbox\Programs\FIESTA\_jaaql.exe
-copy monitor\__init__.py C:\Users\aaron\Dropbox\Programs\FIESTA\jaaql\monitor\__init__.py
-copy monitor\main.py C:\Users\aaron\Dropbox\Programs\FIESTA\jaaql\monitor\main.py
-copy monitor\version.py C:\Users\aaron\Dropbox\Programs\FIESTA\jaaql\monitor\version.py
+copy dist\main.exe %USERPROFILE%\Dropbox\Programs\FIESTA\_jaaql.exe
+copy monitor\__init__.py %USERPROFILE%\Dropbox\Programs\FIESTA\jaaql\monitor\__init__.py
+copy monitor\main.py %USERPROFILE%\Dropbox\Programs\FIESTA\jaaql\monitor\main.py
+copy monitor\version.py %USERPROFILE%\Dropbox\Programs\FIESTA\jaaql\monitor\version.py
 
 rmdir build /s /q
 rmdir dist /s /q
