f7e318b0e14013d43499957d9eeb4f36afdf0baa
Aaron Tasker <aaron.tasker@gmail.com>
2023-10-06 03:20:54 +0200

    Fix

--- build.bat
+++ build.bat
@@ -13,7 +13,7 @@ python setup.py sdist bdist_wheel
 python monitor\version.py>version.txt
 set /p VERSION=<version.txt
 del version.txt
-REM py -m twine upload dist/*
+py -m twine upload dist/*
 
 rmdir jaaql_monitor.egg-info /s /q
 rmdir build /s /q
